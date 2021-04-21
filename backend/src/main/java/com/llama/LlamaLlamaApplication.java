8ce40d34e32ebd445cc20bcba6823e4c42086661
SebsPER <sebasphachi@gmail.com>
2021-04-21 17:10:04 -0500

    Inicializacion del Proyecto

---
+++ backend/src/main/java/com/llama/LlamaLlamaApplication.java
@@ -0,0 +1,13 @@
+package com.llama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlamaLlamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlamaLlamaApplication.class, args);
+    }
+
+}
