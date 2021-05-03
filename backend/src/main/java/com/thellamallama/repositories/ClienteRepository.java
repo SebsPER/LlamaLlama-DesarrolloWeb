fde5cba5b6038e76060b9a21961408fc19901cec
SebsPER <67677649+SebsPER@users.noreply.github.com>
2021-05-03 14:58:38 -0500

    Repositorios de Cliente y Compra
    Consultas DML para entidad Cliente y Compra

--- backend/src/main/java/com/thellamallama/repositories/ClienteRepository.java
+++ backend/src/main/java/com/thellamallama/repositories/ClienteRepository.java
@@ -2,8 +2,13 @@ package com.thellamallama.repositories;
 
 import com.thellamallama.entities.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Optional<Cliente> findById(Long id);
+    Optional<Cliente> findByIdAndPassword(Long id, String password);
+    Optional<Cliente> findByDni(Integer dni);
+    Optional<Cliente> findByTelefono(Integer telefono);
 }
