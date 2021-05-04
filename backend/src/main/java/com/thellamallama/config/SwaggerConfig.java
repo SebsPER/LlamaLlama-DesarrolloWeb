c7938ece100b69bc592412e651498f9e830a38c4
JuniorAlfredoHuerta <u201620846@upc.edu.pe>
2021-05-03 22:45:35 -0500

    "SWAGGER TEST"

--- backend/src/main/java/com/thellamallama/config/SwaggerConfig.java
+++ backend/src/main/java/com/thellamallama/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.thellamallama.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -12,9 +11,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
     public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2 )
+        return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.thellamallama.controllers"))
                 .paths(PathSelectors.any())
