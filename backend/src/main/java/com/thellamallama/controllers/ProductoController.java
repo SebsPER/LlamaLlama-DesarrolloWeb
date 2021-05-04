380ab359308fe90666f5ce5275abb42d23ea4d4a
billbch <80244699+billbch@users.noreply.github.com>
2021-05-03 21:13:04 -0500

    pruduct-Controllers y tienda controllers

---
+++ backend/src/main/java/com/thellamallama/controllers/ProductoController.java
@@ -0,0 +1,46 @@
+package com.thellamallama.controllers;
+
+
+import com.thellamallama.dtos.CreateProductoDto;
+import com.thellamallama.dtos.ProductoDto;
+import com.thellamallama.entities.Producto;
+import com.thellamallama.exceptions.BookingException;
+import com.thellamallama.responses.BookingResponse;
+import com.thellamallama.services.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path="/llamallama"+"/v1")
+public class ProductoController {
+
+
+    @Autowired
+    private ProductoService productoService;
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/product/{productId}")
+    public BookingResponse<ProductoDto> getProductById(@PathVariable Long productoId)
+            throws BookingException{
+        return new BookingResponse<>("Succes",String.valueOf(HttpStatus.OK),"OK",
+                productoService.getProductoById(productoId));
+    }
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/products")
+    public BookingResponse<List<ProductoDto>> getProducts()
+            throws BookingException{
+        return new BookingResponse<>("Succes",String.valueOf(HttpStatus.OK),"OK",
+                productoService.getProductos());
+    }
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/restaurants")
+    public BookingResponse<ProductoDto> createProducto(@RequestBody CreateProductoDto createProductoDto)
+            throws BookingException{
+        return new BookingResponse<>("Succes",String.valueOf(HttpStatus.OK),"OK",
+                productoService.createProducto(createProductoDto));
+    }
+}
---
+++ backend/src/main/java/com/thellamallama/controllers/TiendaController.java
@@ -0,0 +1,46 @@
+package com.thellamallama.controllers;
+
+import com.thellamallama.dtos.CreateTiendaDto;
+import com.thellamallama.dtos.TiendaDto;
+import com.thellamallama.exceptions.BookingException;
+import com.thellamallama.responses.BookingResponse;
+import com.thellamallama.services.TiendaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import lombok.Getter;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path="/booking-restaurant"+"/v1")
+public class TiendaController {
+
+    @Autowired
+    private TiendaService tiendaService;
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/tienda/{tiendaID}")
+    public BookingResponse<TiendaDto> getTiendaById(@PathVariable Long tiendaID) throws BookingException
+    {
+        return new BookingResponse<>("Succes",String.valueOf(HttpStatus.OK),"OK",
+                    tiendaService.getTiendaById(tiendaID));
+
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/tienda")
+    public BookingResponse<List<TiendaDto>> getTiendas()
+        throws BookingException{
+        return new BookingResponse<>("Sucess",String.valueOf(HttpStatus.OK),"OK",
+                tiendaService.getTiendas());
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/restaurant")
+    public BookingResponse<TiendaDto> createTienda(@RequestBody CreateTiendaDto createTiendaDto)
+            throws BookingException{
+        return new BookingResponse<>("Succes",String.valueOf(HttpStatus.OK),"OK",
+                    tiendaService.createTienda(createTiendaDto));
+    }
+}
