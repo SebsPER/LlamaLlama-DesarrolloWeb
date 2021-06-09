263539dc800b314b62a2f16d394fb1a74bb8c766
SebsPER <67677649+SebsPER@users.noreply.github.com>
2021-06-09 18:36:47 -0500

    anotacion @Repository a entidades donde faltaba

--- backend/src/main/java/com/thellamallama/repositories/Compra_ProductoRespository.java
+++ backend/src/main/java/com/thellamallama/repositories/Compra_ProductoRespository.java
@@ -3,9 +3,11 @@ package com.thellamallama.repositories;
 import com.thellamallama.entities.Cliente;
 import com.thellamallama.entities.Compra_Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface Compra_ProductoRespository extends JpaRepository<Compra_Producto,Long> {
     Optional<Compra_Producto> findById(Long id);
     //Optional<Compra_Producto> findByIdAndPassword(Long id, String password);
