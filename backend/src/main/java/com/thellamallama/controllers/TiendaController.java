fd78df3084f2a86b72ce94b7d06932ef8799ed75
SebsPER <67677649+SebsPER@users.noreply.github.com>
2021-06-12 15:14:17 -0500

    Add files via upload

---
+++ backend/src/main/java/com/thellamallama/controllers/TiendaController.java
@@ -0,0 +1,47 @@
+package com.thellamallama.controllers;
+
+import com.thellamallama.dtos.CreateTiendaDto;
+import com.thellamallama.dtos.TiendaDto;
+import com.thellamallama.exceptions.BookingException;
+import com.thellamallama.responses.BookingResponse;
+import com.thellamallama.services.TiendaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import lombok.Getter;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:4200")
+@RequestMapping(path="/llamallama"+"/v1")
+public class TiendaController {
+
+    @Autowired
+    private TiendaService tiendaService;
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/tienda/{tiendaId}")
+    public BookingResponse<TiendaDto> getTiEndabyId(@PathVariable Long tiendaId) throws BookingException
+    {
+        return new BookingResponse<>("Succes",String.valueOf(HttpStatus.OK),"OK",
+                    tiendaService.getTiendabyID(tiendaId));
+
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/tienda")
+    public BookingResponse<List<TiendaDto>> getTiendas()
+        throws BookingException{
+        return new BookingResponse<>("Sucess",String.valueOf(HttpStatus.OK),"OK",
+                tiendaService.getTiendas());
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/tienda")
+    public BookingResponse<TiendaDto> createTienda(@RequestBody CreateTiendaDto createTiendaDto)
+            throws BookingException{
+        return new BookingResponse<>("Succes",String.valueOf(HttpStatus.OK),"OK",
+                    tiendaService.createTienda(createTiendaDto));
+    }
+}
---
+++ backend/src/main/java/com/thellamallama/dtos/CreateProductoDto.java
@@ -0,0 +1,14 @@
+package com.thellamallama.dtos;
+
+import lombok.Getter;
+
+@Getter
+public class CreateProductoDto {
+    private String nombre;
+    private Float precio;
+    private Integer stock;
+    //private Long tiendaid;
+    private Long categoriaid;
+    //private Long compraid;
+}
+
